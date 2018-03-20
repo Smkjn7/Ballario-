d0a2cf31761952d546fd1e93158ae73825a0783d
Samyak Jain <jain.samyak998@gmail.com>
2018-03-21 00:36:03 +0530

    Developent LEVEL 1

---
+++ Assets/Scripts/CameraControl.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var Target:Transform;
+var distance = -10;
+var lift=1.5;
+
+function Update ()
+{
+	transform.position = Target.position+Vector3(0,lift,distance);
+	transform.LookAt(Target);		
+}
